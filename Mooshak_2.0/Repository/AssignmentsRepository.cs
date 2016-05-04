e17790c58ef0ec4bf934bc2518e49b3d3276daad
olafur <olafuralbert@gmail.com>
2016-05-04 16:20:09 +0000

    Assignment klasar
    Bætti við AssignmentViewModels og AssignmentsRepository

---
+++ Mooshak_2.0/Repository/AssignmentsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshak_2._0.Repository
+{
+    public class AssignmentsRepository
+    {
+    }
+}
