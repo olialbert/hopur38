c27c8fc3dcbd60e91beea236ab41f965d0bfd60c
Maríus Sævarsson <Marus Svarsson>
2016-05-04 13:47:29 +0000

    New project

---
+++ Mooshak_2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mooshak_2._0.MvcApplication" Language="C#" %>
---
+++ Mooshak_2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mooshak_2._0.Startup))]
+namespace Mooshak_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
