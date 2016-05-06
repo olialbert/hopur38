74ceb676505f413b739a8e6cf08cb2288cb3109a
sindrie15 <sindrie15@ru.is>
2016-05-06 15:10:36 +0000

    Admin-, Student- and TeacherController

---
+++ Mooshak_2.0/Controllers/TeacherController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshak_2._0.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddAssignment()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateDescription()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteAssignment()
+        {
+            return View();
+        }
+
+        public ActionResult ViewSubmittions()
+        {
+            return View();
+        }
+
+        public ActionResult ViewSubmittionsBestFromAllStudents()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadSubmittionsBestFromAllStudents()
+        {
+            return View();
+        }
+
+        public ActionResult ViewSubmittionBestFromStudent()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadSubmittionBestFromStudent()
+        {
+            return View();
+        }
+
+        public ActionResult ViewSubmittionsBestFromAllStudent()
+        {
+            return View();
+        }
+
+        public ActionResult DownloadSubmittionFromUser()
+        {
+            return View();
+        }
+
+    }
+}
