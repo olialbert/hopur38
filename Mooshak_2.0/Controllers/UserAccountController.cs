5d587af886021c1a8569004982f28ceec3b692ba
sindrie15 <sindrie15@ru.is>
2016-05-11 13:27:10 +0000

    StartOfLogin

---
+++ Mooshak_2.0/Controllers/UserAccountController.cs
@@ -0,0 +1,24 @@
+﻿using Mooshak_2._0.Services;
+using Mooshak_2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mooshak_2._0.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        // GET: UserAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
