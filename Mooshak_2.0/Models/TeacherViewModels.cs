57b576fa9ccd1cfc92d56e3210036fa27e92cbed
peturs15 <peturs15@ru.is>
2016-05-11 13:53:37 +0000

    TeacherViewModel
    TeacherViewModel

--- Mooshak_2.0/Models/TeacherViewModels.cs
+++ Mooshak_2.0/Models/TeacherViewModels.cs
@@ -5,7 +5,14 @@ using System.Web;
 
 namespace Mooshak_2._0.Models
 {
-    public class TeacherViewModels
+    public class AssignmentList
     {
+        public string AssignmentName { get; set; }
+        public List<string> SubAssignments { get; set; }
+    }
+
+    public class TeacherViewModelsAssignmetns
+    {
+        public List<AssignmentList> Assignments { get; set; }
     }
 }
