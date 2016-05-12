53522a421402b3a3674f54eb100cac20f905eb07
sindrie15 <sindrie15@ru.is>
2016-05-12 20:13:45 +0000

    AdminController comments

--- Mooshak_2.0/Controllers/AdminController.cs
+++ Mooshak_2.0/Controllers/AdminController.cs
@@ -18,12 +18,14 @@ namespace Mooshak_2._0.Controllers
             return View();
         }
 
+        //Gets courses from the database using the connectTables and returns them to the View so they can be used
         public ActionResult Courses()
         {
             var Courses = Tables.GetCourses();
             return View(Courses);
         }
         
+        //Gets both the teachers and students from the database using the connectTables, then returns it to the View
         public ActionResult Users()
         {
             var viewModel = new ViewUsersInCourseViewModel();
@@ -39,6 +41,7 @@ namespace Mooshak_2._0.Controllers
             return View();
         }
 
+        //Used to add a course to the database through the view and the connectTable, redirects you to the "Course" View afterwards 
         [HttpPost]
         public ActionResult AddCourse(string Name)
         {
@@ -51,16 +54,16 @@ namespace Mooshak_2._0.Controllers
             return View();
         }
 
+        //Deletes a course via the ID, redirects the user back to the right place in the View
         public ActionResult DeleteCourse(string ID)
         {
             Tables.DeleteCourse(ID);
             return RedirectToAction("Courses");
         }
 
+        //Gets all the users in one singular course, both the teachers and students, from the user table in the database using stored procedure and the connectTable and returns them to the View
         public ActionResult ViewUsersinCourse(string ID)
         {
-
-
             var viewModel = new ViewUsersInCourseViewModel();
             viewModel.CourseName = ID;
             viewModel.Teachers = Tables.GetTeachersInCoursesByName(ID);
@@ -69,6 +72,7 @@ namespace Mooshak_2._0.Controllers
             return View(viewModel);
         }
 
+        //So it's possible to edit/view a user in a special course from the database, the View can then use it
         public ActionResult EditUserViewUserInCourse(string ID)
         {
             List <string> Ids = ID.Split(',').ToList<string>();
@@ -81,8 +85,9 @@ namespace Mooshak_2._0.Controllers
             return View(viewModel);
         }
 
+        //Takes edited information about the the user and sends it to the database adds the user in a new course if needed 
         [HttpPost]
-        public ActionResult EditUserViewUserInCourse(string RedirectId,string SearchName, string Name, string Username, string Password, string Ssn, string Email, string Role, string[] Courses)
+        public ActionResult EditUserViewUserInCourse(string RedirectId, string SearchName, string Name, string Username, string Password, string Ssn, string Email, string Role, string[] Courses)
         {
             int RolesId = Convert.ToInt32(Role);
             int SsnInt = Convert.ToInt32(Ssn);
@@ -98,12 +103,14 @@ namespace Mooshak_2._0.Controllers
             return RedirectToAction("ViewUsersinCourse", new { id = RedirectId });
         }
 
+        //Fetches what course the user is gonna be signed to, using the connectTable
         public ActionResult AddUser()
         {
             var Courses = Tables.GetCourses();
             return View(Courses);
         }
 
+        //Adds a new user to the database from the View 
         [HttpPost]
         public ActionResult AddUser(string Name, string Username, string Password, string Ssn, string Email, string Role, string[] Courses)
         {
@@ -119,12 +126,14 @@ namespace Mooshak_2._0.Controllers
             return RedirectToAction("Users");
         }
 
+        //Fetches the right course name using the ID and returns it
         public ActionResult EditCourse(string ID)
         {
             var Course = Tables.GetCourseByName(ID);
             return View(Course);
         }
 
+        //Updates a course from the View that is already in the database
         [HttpPost]
         public ActionResult EditCourse(string SearchName, string Name)
         {
@@ -132,11 +141,7 @@ namespace Mooshak_2._0.Controllers
             return RedirectToAction("Courses");
         }
 
-        public ActionResult UpdateUser()
-        {
-            return View();
-        }
-
+        //Selects the right user from the View so it can be edited 
         public ActionResult EditUser(string ID)
         {
             var viewModel = new EditUserViewModel();
@@ -145,9 +150,9 @@ namespace Mooshak_2._0.Controllers
             viewModel.UserCourses = Tables.GetCoursesByUser(ID);
             
             return View(viewModel);
-
         }
 
+        //Gets the information that need to be changed from the View and adds them to the database
         [HttpPost]
         public ActionResult EditUser(string SearchName, string Name, string Username, string Password, string Ssn, string Email, string Role, string[] Courses)
         {
@@ -155,37 +160,41 @@ namespace Mooshak_2._0.Controllers
             int SsnInt = Convert.ToInt32(Ssn);
             Tables.UppdateUser(SearchName, RolesId, Name, Username, Password, SsnInt, Email);
             Tables.DeleteUsersToCoursesByUser(Name);
+
             if (Courses.Length != 0)
             {
                 foreach (var Course in Courses)
-                {
-                    
+                {              
                     Tables.AddUsersToCourses(Name, Course);
                 }
             }
-            
+                        
             return RedirectToAction("Users");
         }
 
+        //Delets a student that the user selected from the database using a stored procedure
         public ActionResult DeleteStudent(string ID)
         {
             Tables.DeleteStudent(ID);
             return RedirectToAction("Users");
         }
 
+        //Delets a student that the user selected from the database using a stored procedure, redirects to a different place
         public ActionResult DeleteStudentViewUsersInCourse(string ID)
         {
             List<string> Ids = ID.Split(',').ToList<string>();
             Tables.DeleteStudent(Ids.ElementAt(0));
             return RedirectToAction("ViewUsersinCourse", new { id = Ids.ElementAt(1) });
         }
-
+        
+        //Delets a teacher that the user selected from the database using a stored procedure 
         public ActionResult DeleteTeacher(string ID)
         {
             Tables.DeleteTeacher(ID);
             return RedirectToAction("Users");
         }
 
+        //Delets a teacher that the user selected from the database using a stored procedure, redirects to a different place in the View
         public ActionResult DeleteTeacherViewUsersInCourse(string ID)
         {
             List<string> Ids = ID.Split(',').ToList<string>();
