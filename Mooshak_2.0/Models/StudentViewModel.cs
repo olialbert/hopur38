a7af4e8aba97eff530220d91de91837618958e21
sindrie15 <sindrie15@ru.is>
2016-05-13 17:53:44 +0000

    dass

--- Mooshak_2.0/Models/StudentViewModel.cs
+++ Mooshak_2.0/Models/StudentViewModel.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace Mooshak_2._0.Models
 {
+    //All of these classes are used to get and set variables, they can be used in the View
     public class ViewMySolutionsViewModel
     {
         public string CurrentFile { get; set; }
