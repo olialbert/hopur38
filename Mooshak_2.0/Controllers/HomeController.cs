980130570e70d560b51a3bfb0002af9e91a7105c
olafur <olafuralbert@gmail.com>
2016-05-13 16:14:14 +0000

    Takkar á öll Subbmission

--- Mooshak_2.0/Controllers/HomeController.cs
+++ Mooshak_2.0/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Mooshak_2._0.Controllers
         //So the connectTables can be used
         connectTables Tables = new connectTables();
 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
         //Used to check out what permission the user login in has, redirects the user on the right page depending what role he has in the database
         [HttpPost]
         public ActionResult Index(string UserName, string PassWord)
